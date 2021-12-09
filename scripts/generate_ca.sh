186b763b88a3da4d51842dc97aaf0a29f3250556
TimHsu@M1BP-20210907 <maochindada@gmail.com>
2021-12-09 13:06:03 +0800

    feat(mosquitto): update user name, certs config for mosquitto

--- scripts/generate_ca.sh
+++ scripts/generate_ca.sh
@@ -24,9 +24,12 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer:always
 EOF
 openssl req -nodes -new -x509 -days 3650 -extensions v3_ca -keyout ./configs/certs/ca.key -out ./configs/certs/ca.crt -config ./configs/certs/openssl.cnf
+openssl x509 -inform PEM -in ./configs/certs/ca.crt > ./configs/certs/ca_crt.pem
 openssl genrsa -out ./configs/certs/server.key 2048
+openssl rsa -in ./configs/certs/server.key -text > ./configs/certs/server_key.pem
 openssl req -config ./configs/certs/openssl.cnf -new -key ./configs/certs/server.key -out ./configs/certs/server.csr
 openssl x509 -req -in ./configs/certs/server.csr -CA ./configs/certs/ca.crt -CAkey ./configs/certs/ca.key -CAcreateserial -out ./configs/certs/server.crt -days 3650
+openssl x509 -inform PEM -in ./configs/certs/server.crt > ./configs/certs/server_crt.pem
 
 touch ./configs/certs/clientssl.cnf
 cat >> ./configs/certs/clientssl.cnf <<EOF
@@ -49,8 +52,10 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer:always
 EOF
 openssl genrsa -out ./configs/certs/client.key 2048
+openssl rsa -in ./configs/certs/client.key -text > ./configs/certs/client_key.pem
 openssl req -config ./configs/certs/clientssl.cnf -new -key ./configs/certs/client.key -out ./configs/certs/client.csr
 openssl x509 -req -in ./configs/certs/client.csr -CA ./configs/certs/ca.crt -CAkey ./configs/certs/ca.key -CAcreateserial -out ./configs/certs/client.crt -days 3650
+openssl x509 -inform PEM -in ./configs/certs/client.crt > ./configs/certs/client_crt.pem
 
 openssl dhparam -out ./configs/certs/dhparam.pem 2048
 mv ./.srl ./configs/certs
